7f08609e7219546673b5ec63998bb0af6e48f8cf
Vignesh <Vignesh@VIGNESH-PC>
2016-05-11 20:51:35 +0530

    Added documentation and separated code

--- XposedModuleScraper/XposedModuleScraper/Program.cs
+++ XposedModuleScraper/XposedModuleScraper/Program.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Net;
 using System.Diagnostics;
 using System.Threading;
+using System.Net;
 
 namespace XposedModuleScraper
 {
@@ -14,57 +15,55 @@ namespace XposedModuleScraper
     {
         static void Main(string[] args)
         {
-            string FirstPageSource;
+            #region Declarations
+            //strings
+            string FirstPageSource;                         //HTML Source Code for first page. We can get number of modules from this string.
+            string temp1, temp2, temp3, temp4;              //temporary strings to perform string operations.
             string UrlTemplate = "http://repo.xposed.info/module-overview?combine=&status=All&field_restrict_edits_value=All&sort_by=field_last_update_value&page=";
-            List<string> htmlPages = new List<string>();
-            FirstPageSource = new System.Net.WebClient().DownloadString(UrlTemplate + 0);
+            //List<string>
+            List<string> htmlPages = new List<string>();    // one for pages which list 10 modules per page.
+            List<string> appIDNamesUnFormatted = new List<string>();   // one for 
+            Dictionary<string, string> appIdNames = new Dictionary<string, string>();
+            //integers
+            int pages;                                      //number of pages.
+            int modules;                                    //number of modules.
+            int i;                                          //iteration variable.
+            #endregion
+            #region Get First Page Source and number of modules
+            FirstPageSource = new WebClient().DownloadString(UrlTemplate + 0);
             htmlPages.Add(FirstPageSource);
-            int modules = Convert.ToInt32(FirstPageSource.Substring(FirstPageSource.IndexOf("Displaying 1 - 10 of") + 20, 18).Remove(FirstPageSource.Substring(FirstPageSource.IndexOf("Displaying 1 - 10 of") + 20, 18).IndexOf(" mod")).Trim());
-            int pages;
+            modules = Convert.ToInt32(FirstPageSource.Substring(FirstPageSource.IndexOf("Displaying 1 - 10 of") + 20, 18).Remove(FirstPageSource.Substring(FirstPageSource.IndexOf("Displaying 1 - 10 of") + 20, 18).IndexOf(" mod")).Trim());
+
+#if DEBUG
             modules = Convert.ToInt32(Console.ReadLine());
-            if((modules % 10) == 0)
-            {
+
+#endif
+            #endregion
+            #region Determine number of Pages
+            if ((modules % 10) == 0)
                 pages = modules / 10;
-            }
             else
-            {
                 pages = ((modules - (modules % 10)) / 10) + 1;
-            }
-
-            
-
-
-            for (int i=0; i < 100;i++)
+            #endregion
+            #region retrieve all pages
+            // retrieve all pages each of which displays 10 modules including the last page which can display 1,2,or even 10 modules
+            for (i=1; i <= pages;i++)
             {
                 htmlPages.Add(new System.Net.WebClient().DownloadString(UrlTemplate + i));
             }
-            
+            #endregion
 
-            string temp1, temp2, temp3, temp4;
-            List<string> appIDNames = new List<string>();
-            for(int i=0;i< 100;i++)
+            for (i=0;i< 100;i++)
             {
 
                 temp1 = htmlPages.ElementAt(i).Substring(htmlPages.ElementAt(i).IndexOf(@"<tbody>") + 7, htmlPages.ElementAt(i).IndexOf("</tbody>"));
                 temp2 = temp1.Substring(temp1.IndexOf(@"<a href=""/module/"""), temp1.IndexOf("</a>"));
                 temp1 = temp1.Substring(temp1.IndexOf("</a>"));
-                appIDNames.Add(temp1);
+                appIDNamesUnFormatted.Add(temp1);
 
-
-
-
-
-
-                if (i > (modules / 10) - 5)
-                {
-                    string fa = htmlPages.ElementAt(i);
-                    if (!(fa.Contains(@"<div class=""view-content"">""")))
-                    {
-                        htmlPages.RemoveAt(i);
-                        break;
-                    }
-                }
+                
             }
+            
 
 
 
