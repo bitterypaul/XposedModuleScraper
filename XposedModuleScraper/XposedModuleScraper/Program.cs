64ef2e1a0cd05dee6d32ade0d30fd34abfb9effa
Vignesh <Vignesh@VIGNESH-PC>
2016-05-11 21:24:03 +0530

    Make code more pretty and added time delay between requests to avoid robots.txt

--- XposedModuleScraper/XposedModuleScraper/Program.cs
+++ XposedModuleScraper/XposedModuleScraper/Program.cs
@@ -13,12 +13,13 @@ namespace XposedModuleScraper
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             #region Declarations
             //strings
             string FirstPageSource;                         //HTML Source Code for first page. We can get number of modules from this string.
-            string temp1, temp2, temp3, temp4;              //temporary strings to perform string operations.
+            string temp1, temp2;                            //temporary strings to perform string operations.
             string UrlTemplate = "http://repo.xposed.info/module-overview?combine=&status=All&field_restrict_edits_value=All&sort_by=field_last_update_value&page=";
             //List<string>
             List<string> htmlPages = new List<string>();    // one for pages which list 10 modules per page.
@@ -28,18 +29,14 @@ namespace XposedModuleScraper
             int pages;                                      //number of pages.
             int modules;                                    //number of modules.
             int i;                                          //iteration variable.
+            int delayBetweenRequests = 1;                       //Time delay between successive requests to prevent being locked out from the system
+            //Objects
+            WebClient webClient = new WebClient();          //Declare WebClient Object
             #endregion
-            #region Get First Page Source and number of modules
-            FirstPageSource = new WebClient().DownloadString(UrlTemplate + 0);
+            #region Get First Page Source determine number of modules and pages
+            FirstPageSource = webClient.DownloadString(UrlTemplate + 0);
             htmlPages.Add(FirstPageSource);
             modules = Convert.ToInt32(FirstPageSource.Substring(FirstPageSource.IndexOf("Displaying 1 - 10 of") + 20, 18).Remove(FirstPageSource.Substring(FirstPageSource.IndexOf("Displaying 1 - 10 of") + 20, 18).IndexOf(" mod")).Trim());
-
-#if DEBUG
-            modules = Convert.ToInt32(Console.ReadLine());
-
-#endif
-            #endregion
-            #region Determine number of Pages
             if ((modules % 10) == 0)
                 pages = modules / 10;
             else
@@ -49,12 +46,16 @@ namespace XposedModuleScraper
             // retrieve all pages each of which displays 10 modules including the last page which can display 1,2,or even 10 modules
             for (i=1; i <= pages;i++)
             {
-                htmlPages.Add(new System.Net.WebClient().DownloadString(UrlTemplate + i));
+                htmlPages.Add(webClient.DownloadString(UrlTemplate + i));
+                Thread.Sleep(delayBetweenRequests);
             }
             #endregion
 
+
+
             for (i=0;i< 100;i++)
             {
+               
 
                 temp1 = htmlPages.ElementAt(i).Substring(htmlPages.ElementAt(i).IndexOf(@"<tbody>") + 7, htmlPages.ElementAt(i).IndexOf("</tbody>"));
                 temp2 = temp1.Substring(temp1.IndexOf(@"<a href=""/module/"""), temp1.IndexOf("</a>"));
