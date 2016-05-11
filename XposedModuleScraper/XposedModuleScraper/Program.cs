a10856330abeba823e19befacfee8130186ecaa7
Vignesh <Vignesh@VIGNESH-PC>
2016-05-11 13:52:50 +0530

    Initial Commit

---
+++ XposedModuleScraper/XposedModuleScraper/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace XposedModuleScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            WebRequest req = HttpWebRequest.Create("http://repo.xposed.info/module-overview");
+            req.Method = "GET";
+
+            string source;
+            using (StreamReader reader = new StreamReader(req.GetResponse().GetResponseStream()))
+            {
+                source = reader.ReadToEnd();
+            }
+
+            Console.WriteLine(source);
+            Console.Read();
+        }
+    }
+}
