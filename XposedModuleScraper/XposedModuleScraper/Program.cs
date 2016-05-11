543eb99ad5e554ae38e728d189f5464d76b197ed
Vignesh <Vignesh@VIGNESH-PC>
2016-05-11 15:32:07 +0530

    Initial Commit

--- XposedModuleScraper/XposedModuleScraper/Program.cs
+++ XposedModuleScraper/XposedModuleScraper/Program.cs
@@ -12,19 +12,54 @@ namespace XposedModuleScraper
     {
         static void Main(string[] args)
         {
+            string FirstPageSource;
+            // Get and process HTML Code of first page
+            #region first page HTML Code
+            //FirstPageSource = GetHTMLSource("http://repo.xposed.info/module-overview");
+            FirstPageSource = new System.Net.WebClient().DownloadString("http://repo.xposed.info/module-overview");
+            //Process the HTML Code
+
+            // create a easy to type variable name for convenience
+            string a = FirstPageSource;
+            int a1 = a.IndexOf("Displaying 1 - 10 of") + 20;
+            string astr = a.Substring(a1, 18);
+            string astr1 = astr.Remove(astr.IndexOf(" mod"));
+            string astr2 = astr1.Trim();
+            int modules = Convert.ToInt32(astr2);
 
 
-            WebRequest req = HttpWebRequest.Create("http://repo.xposed.info/module-overview");
+            #endregion
+            string UrlTemplate = "http://repo.xposed.info/module-overview?combine=&status=All&field_restrict_edits_value=All&sort_by=field_last_update_value&page=";
+
+
+
+
+#if DEBUG
+            System.Console.WriteLine(FirstPageSource);
+            Console.WriteLine("\n\n\n\n\n" + modules);
+            Console.ReadLine();
+#endif
+        }
+        public static string GetHTMLSource(string Url)
+        {
+            string htmlCode;
+            WebRequest req = HttpWebRequest.Create(Url);
             req.Method = "GET";
 
-            string source;
             using (StreamReader reader = new StreamReader(req.GetResponse().GetResponseStream()))
             {
-                source = reader.ReadToEnd();
+                htmlCode = reader.ReadToEnd();
             }
+            return htmlCode;
+        }
+        public static string[] GetSequentialHTMLPages(string Url, int pagesLimit)
+        {
+            string[] htmlPages = new string[pagesLimit];
+            for(int i=1; i<pagesLimit; i++)
+            {
 
-            Console.WriteLine(source);
-            Console.Read();
+            }
+            return htmlPages;
         }
     }
 }
